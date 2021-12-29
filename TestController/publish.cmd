89d616e2404832f4cf7fa1ec19447102b3fe0431
Tomas Jecha <tomas@jecha.net>
2021-12-29 15:14:20 +0100

    Fix address of server.

--- TestController/publish.cmd
+++ TestController/publish.cmd
@@ -1,2 +1,2 @@
 dotnet publish -r linux-arm --no-self-contained
-scp -r .\bin\Debug\net6.0\linux-arm\publish\* pi@hehohub:/home/pi/led-screen/
+scp -r .\bin\Debug\net6.0\linux-arm\publish\* pi@hehohub.lan:/home/pi/led-screen/
